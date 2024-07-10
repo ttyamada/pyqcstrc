388ec126ad8c00f4a98147f4048adfc9fb9a35cd
ttyamada <49137028+ttyamada@users.noreply.github.com>
2024-07-10 18:13:20 +0900

    add site_symmetry()

--- src/pyqcstrc/ico2/symmetry.py
+++ src/pyqcstrc/ico2/symmetry.py
@@ -17,6 +17,8 @@ from pyqcstrc.ico2.utils import (remove_doubling_in_perp_space,
                                 )
 import numpy as np
 
+EPS=1e-6
+
 def symop_obj(symop,obj,centre):
     """ Apply a symmetric operation on an object around given centre. in TAU-style
     
@@ -171,6 +173,279 @@ def icosasymop():
                         symop.append(tmp)
     return symop
 
+####### WIP ########
+################ 
+# site symmetry
+################
+def site_symmetry(site):
+    """symmetry operators in the site symmetry group G.
+    
+    Args:
+        site (numpy.ndarray):
+            xyz coordinate of the site.
+            The shape is (6,3).
+            
+    Returns:
+        List of index of symmetry operators of the site symmetry group G (list):
+            The symmetry operators leaves xyz identical.
+    """
+    
+    symop=icosasymop()
+    traop=translation()
+    
+    list1=[]
+    for i2,op in enumerate(symop):
+        site1=symop_vec(op,site,V0)
+        flag=0
+        for top in traop:
+            site2=add_vectors(site1,top)
+            tmp=sub_vectors(site,site2)
+            if length_numerical(tmp)<EPS:
+                list1.append(i2)
+                break
+            else:
+                pass
+    return remove_overlaps(list1)
+
+def coset(site):
+    """coset
+    """
+    symop=icosasymop()
+    
+    list1=site_symmetry(site)
+    
+    # List of index of symmetry operators which are not in the G.
+    tmp=range(len(symop))
+    tmp=set(tmp)-set(list1)
+    list2=list(tmp)
+    
+    list2_new=remove_overlaps(list2)
+    
+    if len(symop)==len(list1):
+        list5=[0]
+    else:
+        # left coset decomposition:
+        list4=[]
+        for i2 in list2_new:
+            list3=[]
+            for i1 in list1:
+                op1=np.dot(symop[i2],symop[i1])
+                for i3,op in enumerate(symop):
+                    if np.all(op1==op):
+                        list3.append(i3)
+                        break
+                    else:
+                        pass
+            list4.append(list3)
+        
+        for i2 in range(len(list4)-1):
+            a=list4[i2]
+            b=[]
+            d=[]
+            list5=[0] # symmetry element of identity, symop[0]
+            list5.append(list2_new[i2])
+            i3=i2+1
+            while i3<len(list4):
+                b=list4[i3]
+                if len(d)==0:
+                    if find_overlaps(a,b):
+                        pass
+                    else:
+                        d=a+b
+                        list5.append(list2_new[i3])
+                else:
+                    if find_overlaps(d,b):
+                        pass
+                    else:
+                        d=d+b
+                        list5.append(list2_new[i3])
+                i3+=1
+            b=remove_overlaps(d)
+            if len(symop)==len(list5)*len(list1):
+                break
+            else:
+                pass
+    return list5
+
+def site_symmetry_and_coset(site):
+    #symmetry operators in the site symmetry group G and its left coset decomposition.
+    #
+    #Args:
+    #    site (numpy.ndarray):
+    #        xyz coordinate of the site.
+    #        The shape is (6,3).
+    #
+    #Returns:
+    #    List of index of symmetry operators of the site symmetry group G (list):
+    #        The symmetry operators leaves xyz identical.
+    #    
+    #    List of index of symmetry operators in the left coset representatives of the poibt group G (list):
+    #        The symmetry operators generates equivalent positions of the site xyz.
+    
+    symop=icosasymop()
+    traop=translation()
+    
+    # List of index of symmetry operators of the site symmetry group G.
+    # The symmetry operators leaves xyz identical.
+    list1=[]
+    
+    # List of index of symmetry operators which are not in the G.
+    list2=[]
+    
+    for i2,op in enumerate(symop):
+        site1=symop_vec(op,site,V0)
+        flag=0
+        for top in traop:
+            site2=add_vectors(site1,top)
+            tmp=sub_vectors(site,site2)
+            if length_numerical(tmp)<EPS:
+                list1.append(i2)
+                flag+=1
+                break
+            else:
+                pass
+        if flag==0:
+            list2.append(i2)
+    
+    list1_new=remove_overlaps(list1)
+    list2_new=remove_overlaps(list2)
+    
+    if len(symop)==len(list1_new):
+        list5=[0]
+    else:
+        # left coset decomposition:
+        list4=[]
+        for i2 in list2_new:
+            list3=[]
+            for i1 in list1_new:
+                op1=np.dot(symop[i2],symop[i1])
+                for i3,op in enumerate(symop):
+                    if np.all(op1==op):
+                        list3.append(i3)
+                        break
+                    else:
+                        pass
+            list4.append(list3)
+        
+        for i2 in range(len(list4)-1):
+            a=list4[i2]
+            b=[]
+            d=[]
+            list5=[0] # symmetry element of identity, symop[0]
+            list5.append(list2_new[i2])
+            i3=i2+1
+            while i3<len(list4):
+                b=list4[i3]
+                if len(d)==0:
+                    if find_overlaps(a,b):
+                        pass
+                    else:
+                        d=a+b
+                        list5.append(list2_new[i3])
+                else:
+                    if find_overlaps(d,b):
+                        pass
+                    else:
+                        d=d+b
+                        list5.append(list2_new[i3])
+                i3+=1
+            b=remove_overlaps(d)
+            if len(symop)==len(list5)*len(list1_new):
+                break
+            else:
+                pass
+    
+    return list1_new,list5
+
+#################
+#   Utilities
+#################
+def remove_overlaps(l1):
+    """
+    Remove overlap elements in list with set method.
+    
+    Args:
+        l1 (list):
+    
+    Returns:
+        l2 (list)
+    """
+    tmp=set(l1)
+    l2=list(tmp)
+    l2.sort()
+    return l2
+    
+def find_overlaps(l1,l2):
+    """find overlap or not btween list1 and list2.
+    
+    Args:
+        l1 (list):
+        l2 (list):
+    
+    Returns:
+        True : overlaping
+        False: no overlap
+    """
+    l3=remove_overlaps(l1+l2)
+    if len(l1)+len(l2)==len(l3): # no overlap
+        return False
+    else:
+        return True
+
+def translation():
+    """translational symmetry
+    """
+    symop=[]
+    lst=[-1,0,1]
+    tmp=np.array([[0,0,1],[0,0,1],[0,0,1],[0,0,1],[0,0,1],[0,0,1]])
+    symop.append(tmp)
+    for i1 in lst:
+        for i2 in lst:
+            for i3 in lst:
+                for i4 in lst:
+                    for i5 in lst:
+                        for i6 in lst:
+                            tmp=np.array([[i1,0,1],[i2,0,1],[i3,0,1],[i4,0,1],[i5,0,1],[i6,0,1]])
+                            symop.append(tmp)
+    return symop
+
+############################
+# Similarity transformation
+############################
+def similarity_obj(obj,m):
+    """similarity transformation of a triangle
+    """
+    out=np.zeros(obj.shape,dtype=np.int64)
+    for i1,od in enumerate(obj):
+        out[i1]=similarity_triangle(od,m)
+    return out
+
+def similarity_triangle(triangle,m):
+    """similarity transformation of a triangle
+    """
+    out=np.zeros(triangle.shape,dtype=np.int64)
+    for i1,vt in enumerate(triangle):
+        out[i1]=similarity_vec(vt,m)
+    return out
+
+def similarity_vec(vt,m):
+    """similarity transformation of a vector
+    """
+    vec1=[]
+    op=similarity(m)
+    return dot_product_1(op,vt)
+    
+def similarity(m):
+    """Similarity transformation of icosahedral QCs
+    """
+    m1=0.5*np.array([[1, 1, 1, 1, 1, 1],\
+                    [ 1, 1, 1,-1,-1, 1],\
+                    [ 1, 1, 1, 1,-1,-1],\
+                    [ 1,-1, 1, 1, 1,-1],\
+                    [ 1,-1,-1, 1, 1, 1],\
+                    [ 1, 1,-1,-1, 1, 1]],dtype=np.int64)
+    return matrixpow(m1.T,m)
+    
 if __name__ == '__main__':
     
     # test
