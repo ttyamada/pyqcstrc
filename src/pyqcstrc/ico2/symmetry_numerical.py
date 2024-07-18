1b8bd1a180b17d713622e2c0709b983074122e4f
ttyamada <49137028+ttyamada@users.noreply.github.com>
2024-07-18 17:45:49 +0900

    Update symmetry_numerical.py

--- src/pyqcstrc/ico2/symmetry_numerical.py
+++ src/pyqcstrc/ico2/symmetry_numerical.py
@@ -38,34 +38,65 @@ def symop_vec(symop,vn,centre):
 def symop_vecs(symop,vns,centre):
     """ Apply a symmetric operation on set of vectors around given centre.
     
+    Parameters
+    ----------
+    symop: list
+        list of symmetry operations
+    vns: array
+        6D cectors
+    centre: array
+        symmetrical centre
+    
+    Returns
+    -------
+    vec: array
+        vector
     """
-    shape=tuple([len(symop)])
-    out=np.zeros(shape+vns.shape,dtype=np.float64)
-    for i1,op in enumerate(symop):
-        for i2,vn in enumerate(vns):
-            out[i1][i2]=symop_vec(op,vn,centre)
-    return out
+    if vns.ndim==2:
+        shape=tuple([len(symop)])
+        out=np.zeros(shape+vns.shape,dtype=np.float64)
+        for i1,op in enumerate(symop):
+            for i2,vn in enumerate(vns):
+                out[i1][i2]=symop_vec(op,vn,centre)
+        return out
+    else:
+        print('vns has an incorrect shape!')
+        return
 
 def symop_obj(symop,obj,centre):
     """ Apply a symmetric operation on an object around given centre.
     
+    
+    Parameters
+    ----------
+    symop: list
+        list of symmetry operations
+    obj: array
+        6D cectors
+    centre: array
+        symmetrical centre
+    
+    Returns
+    -------
+    vec: array
+        vector
     """
     ndim=obj.ndim
-    if ndim==4:
+    if ndim==3:
         shape=tuple([len(symop)])
         out=np.zeros(shape+obj.shape,dtype=np.float64)
         for i1,op in enumerate(symop):
             for i2,vns in enumerate(obj):
-                out[i1][i2]=symop_vecs(op,vns,centre)
+                for i3,vn in enumerate(vns):
+                    out[i1][i2][i3]=symop_vec(op,vn,centre)
         return out
-    elif ndim==3:
-        return symop_vecs(symop,obj,centre)
     elif ndim==2:
+        return symop_vecs(symop,obj,centre)
+    elif ndim==1:
         return symop_vec(symop,obj,centre)
     else:
         print('object has an incorrect shape!')
         return 
-
 
 def generator_obj_symmetric_vector_specific_symop(vn,centre,list_of_symmetry_operation_index):
     # using specific symmetry operations
@@ -75,6 +106,24 @@ def generator_obj_symmetric_vector_specific_symop(vn,centre,list_of_symmetry_ope
         for i1 in list_of_symmetry_operation_index:
             symop.append(mop[i1])
         return symop_vec(symop,vn,centre)
+    else:
+        print('vn has an incorrect shape!')
+        return
+
+def generator_obj_symmetric_vectors_specific_symop(vns,centre,list_of_symmetry_operation_index):
+    # using specific symmetry operations
+    if vns.ndim==2:
+        mop=icosasymop()
+        symop=[]
+        for i1 in list_of_symmetry_operation_index:
+            symop.append(mop[i1])
+        return symop_vecs(symop,vns,centre)
+    elif vns.ndim==3:
+        mop=icosasymop()
+        symop=[]
+        for i1 in list_of_symmetry_operation_index:
+            symop.append(mop[i1])
+        return symop_obj(symop,vns,centre)
     else:
         print('vn has an incorrect shape!')
         return
@@ -132,7 +181,8 @@ def site_symmetry(site,brv):
             site2=add_vectors(site1,top)
             tmp=sub_vectors(site,site2)
             #if length_numerical(tmp)<EPS:
-            if np.sqrt(tmp[0]**2+tmp[1]**2+tmp[2]**2+tmp[3]**2+tmp[4]**2+tmp[5]**2)<EPS:
+            #if np.sqrt(tmp[0]**2+tmp[1]**2+tmp[2]**2+tmp[3]**2+tmp[4]**2+tmp[5]**2)<EPS:
+            if np.linalg.norm(tmp)<EPS:
                 list1.append(i2)
                 break
             else:
@@ -231,7 +281,8 @@ def site_symmetry_and_coset(site,brv):
             site2=site1+top
             tmp=site-site2
             #if length_numerical(tmp)<EPS:
-            if np.sqrt(tmp[0]**2+tmp[1]**2+tmp[2]**2+tmp[3]**2+tmp[4]**2+tmp[5]**2)<EPS:
+            #if np.sqrt(tmp[0]**2+tmp[1]**2+tmp[2]**2+tmp[3]**2+tmp[4]**2+tmp[5]**2)<EPS:
+            if np.linalg.norm(tmp)<EPS:
                 list1.append(i2)
                 flag+=1
                 break
@@ -315,7 +366,77 @@ def similarity_vector(vn,m):
     op=similarity(m)
     return np.dot(op,vn)
     
-
-
-
-
+if __name__ == '__main__':
+    
+    # test
+    
+    import random
+    from numericalc import (numerical_vectors,
+                            numerical_vector,
+                            numeric_value,)
+                            
+    def generate_random_value():
+        """ generate value in TAU-style
+        """
+        nmax=10
+        v=np.zeros((3),dtype=np.int64)
+        for i1 in range(2):
+            v[i1]=random.randrange(-nmax,nmax) # a and b in (a+b*TAU)/c.
+        v[2]=random.randrange(1,nmax) # c in (a+b*TAU)/c.
+        return v
+        
+    def generate_random_vector(ndim=6):
+        """ generate ndim vector in TAU-style
+        ndim: dimension of vectors
+        """
+        nmax=10
+        v=np.zeros((ndim,3), dtype=np.int64)
+        for i1 in range(ndim):
+            v[i1]=generate_random_value()
+        return v
+        
+    def generate_random_vectors(n,ndim=6):
+        """
+        num: number of generated vectors.
+        ndim: dimension of vectors
+        """
+        v=np.zeros((n,ndim,3), dtype=np.int64)
+        for i1 in range(n):
+            v[i1]=generate_random_vector(ndim)
+        return v
+    
+    def generate_random_tetrahedron():
+        return generate_random_vectors(4)
+    
+    cen0=np.array([[0,0,1],[0,0,1],[0,0,1],[0,0,1],[0,0,1],[0,0,1]])
+    
+    
+    print("TEST: symop_vec()")
+    symop=icosasymop()
+    vt=generate_random_vector()
+    counter=0
+    for sop in symop:
+        #
+        # calc using symop_vec
+        svt=symop_vec(sop,vt,cen0)
+        svn1=numerical_vector(svt)
+        #print(svn1)
+        #
+        # calc using no.dot with float values
+        vn=numerical_vector(vt)
+        svn2=np.dot(sop,vn)
+        #print(svn2)
+        if np.allclose(svn1,svn2):
+            pass
+        else:
+            counter+=1
+    if counter==0:
+        print('symop_vec: correct')
+    else:
+        print('symop_vec: worng')
+        
+    nset=4
+    vts=generate_random_vectors(nset)
+    print(vts)
+    svts=symop_vecs(symop[1],vts,cen0)
+    print(svts)
