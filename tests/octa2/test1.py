46c0aa426bb43280335fa4d2ce6e6bca8f759ff7
ttyamada <49137028+ttyamada@users.noreply.github.com>
2024-11-23 12:16:58 +0900

    Update test1.py

--- tests/octa2/test1.py
+++ tests/octa2/test1.py
@@ -5,22 +5,31 @@
 # Copyright (c) 2021 Tsunetomo Yamada <tsunetomo.yamada@rs.tus.ac.jp>
 
 #import timeit
-#import os
+import os
 import sys
 import numpy as np
 import pyqcstrc.octa2.occupation_domain as od
 import pyqcstrc.octa2.two_occupation_domains as ods
 
 opath='./test1'
-
+try:
+    os.makedirs(opath)
+except FileExistsError:
+    pass
+    
 # Three 6D vectors which define the asymmetric part of the occupation domain of Amman-Binker octagonal tiling.
 # Note that 5-th and 6-th components of each 6D vectors are dummy, and they correspond to Z coordinate in Epar and Eperp, respectively.
 v0=np.array([[ 0, 0, 1],[ 0, 0, 1],[ 0, 0, 1],[ 0, 0, 1],[ 0, 0, 1],[ 0, 0, 1]])
 v1=np.array([[ 1, 0, 1],[ 0, 0, 1],[ 0, 0, 1],[ 0, 0, 1],[ 0, 0, 1],[ 0, 0, 1]]) # (1,0,0,0)
 v2=np.array([[ 1, 0, 2],[ 0, 0, 1],[ 0, 0, 1],[ 1, 0, 2],[ 0, 0, 1],[ 0, 0, 1]]) # (1,0,0,1)/2
 od_asym=np.vstack([v0,v1,v2]).reshape(1,3,6,3)
-od.write(obj=od_asym, path=opath, basename='od_1_asym', format='xyz')
+
+# Output 
 od.write(obj=od_asym, path=opath, basename='od_1_asym', format='vesta', color='r',select='normal')
+od.write(obj=od_asym, path=opath, basename='od_1_asym', format='xyz')
+
+# Read XYZ file
+od_asym=od.read_xyz(path=opath,basename='od_1_asym',select='triangle')
 
 #============================================
 # OBJ_1 at (0,0,0,0)
