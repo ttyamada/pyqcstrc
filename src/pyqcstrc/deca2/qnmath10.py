f1671d8240f3379781d65b9ade2a86aa144fcd60
Akiji Yamamoto <ayamamotojpn@ybb.ne.jp>
2025-02-16 19:04:32 +0900

    src/pyqcstrc/* moved to src

--- src/pyqcstrc/deca2/qnmath10.py
+++
@@ -1,4 +0,0 @@
-class Qnmath10(Qnmath):
-    def __init__(self):
-        super().__init__(5) # use N=3 SQRTN=np.sqrt(5)
-    
--- src/pyqcstrc/octa2/qnmath8.py
+++
@@ -1,4 +0,0 @@
-class Qnmath8(Qnmath):
-    def __init__(self):
-        super().__init__(2) # use N=3 SQRTN=np.sqrt(2)
-    
