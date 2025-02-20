9163e8de2d465871dc62aa7f6d6b7c231609fcdb
Akiji Yamamoto <ayamamotojpn@ybb.ne.jp>
2025-02-20 11:44:27 +0900

    test1.sh test2.sh in tests/icos added

---
+++ tests/ico2/tests/test1.sh
@@ -0,0 +1,3 @@
+export PYTHONPATH=/home/akiji/git/pyqcstrc/tests/ico2/tests
+# for profiling
+python -m cProfile -o test1.pstats test1.py
---
+++ tests/ico2/tests/test2.sh
@@ -0,0 +1,3 @@
+export PYTHONPATH=/home/akiji/git/pyqcstrc/tests/ico2/tests
+# for profiling
+python -m cProfile -o test2.pstats test2.py
