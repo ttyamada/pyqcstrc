f445e3bb19f090f94606a70cd4e54608cb33722d
ttyamada <49137028+ttyamada@users.noreply.github.com>
2026-01-05 16:18:38 +0900

    Update strc.py

--- src/pyqcstrc/ico2/strc.py
+++ src/pyqcstrc/ico2/strc.py
@@ -58,10 +58,208 @@ except ImportError:
 TAU=(1+np.sqrt(5))/2.0
 EPS=1e-6
 V0=np.array([[0,0,1],[0,0,1],[0,0,1],[0,0,1],[0,0,1],[0,0,1]],dtype=np.int64)
-V1=np.array([0, 0, 0, 0, 0, 0],dtype=np.float64)
-V2=np.array([0, 0, 0],dtype=np.float64)
+V1=np.array([0,0,0,0,0,0],dtype=np.float64)
+V2=np.array([0,0,0],dtype=np.float64)
 CONST1 = 1/np.sqrt(2.0+TAU)
 
+def gen_window(aico, brv, model, x1, x2, x3, verbose = 0, test_flag = 0):
+    """
+    generating window from the input model.
+    """
+    
+    x1=np.array(x1,dtype=np.float64) #5f
+    x2=np.array(x2,dtype=np.float64) #3f
+    x3=np.array(x3,dtype=np.float64) #2f
+    
+    if brv=='s':
+        aico=aico*2
+    else:
+        pass
+        
+    oshift=projection3_numerical(np.array(oshift,dtype=np.float64))
+    if brv=='s':
+        oshift=oshift/2
+        
+    print('Generating nD structure...')
+    lst_shape=[]
+    lst_objs=[]
+    lst_pos=[]
+    lst_atm=[]
+    lst_be=[]
+    lst_occ=[]
+    lst_rmax=[]
+    lst_mu=[]
+    lst_xe1=[]
+    lst_xe2=[]
+    lst_xe3=[]
+    lst_mxe1=[]
+    lst_mxe2=[]
+    lst_mxe3=[]
+    lst_eshift=[]
+    
+    lst_radius_spherical_obj=[]
+    
+    #### TEST ####
+    if test_flag==0:
+        pass
+    else:
+        lst_6dspin=[]
+        lst_mx1=[]
+        lst_mx2=[]
+        lst_mx3=[]
+    ##############
+    
+    for i1,nod in enumerate(model):
+        atom, pod, position, eshift, be, occ, rmax, mu = model[nod]
+        obj=pod[1]
+        if brv=='s':
+            obj=mul_vectors(obj,np.array([1,0,2]))
+            position=mul_vector(position,np.array([1,0,2]))
+        else:
+            pass
+        indx_site_sym,indx_coset=site_symmetry_and_coset(position,brv,verbose)
+        
+        #========================================================================
+        # generate positions of ODs necessary to generate atomic positions.
+        #========================================================================
+        vtss=equivalent_sites_with_centring(position,brv,indx_coset)
+        n1,n2,_,_=vtss.shape
+        vnss=np.zeros((n1,n2,6),dtype=np.float64)
+        for i2,vts in enumerate(vtss):
+            vnss[i2]=numerical_vectors(vts)
+        if verbose>0:
+            print('  equivalent positions:')
+            for i2,vns in enumerate(vnss):
+                for i3,vn in enumerate(vns):
+                    print('    %d-%d:'%(i2+1,i3+1),vn)
+        else:
+            pass
+            
+        if pod[0]=='polyhedron'
+            if pod[2]==1: # asymmetric ODs
+                # generating i1-th symmetric occupation domain from its asymmetric unit.
+                obj=generator_obj_symmetric_obj_specific_symop(obj,V0,indx_site_sym)
+            else: # symmetric ODs
+                pass
+                
+            # Spherical approximation of the OD (tmp) to a spherical OD.
+            radius_spherical_obj = spherical_approximation_obj(obj)
+            print('   radius of spherically approximated obj: %8.6f, %8.6f'%(radius_spherical_obj[0],radius_spherical_obj[1]))
+            lst_radius_spherical_obj.append(radius_spherical_obj)
+            
+            # Generating equivalent ODs in unit cell
+            objs1=generator_obj_symmetric_obj_specific_symop(obj,V0,indx_coset)
+            
+            # here, each TAU-style value is transformed to numerical one.
+            n1,n2,n3,_,_,_=objs1.shape
+            objs1_=np.zeros((n1,n2,n3,4,3),dtype=np.float64)
+            for j2 in range(n1):
+                for j3 in range(n2):
+                    for j4 in range(n3):
+                        objs1_[j2][j3][j4]=get_internal_component_sets_numerical(objs1[j2][j3][j4])
+                        
+        else: # shperical, shell, elipsolid OD
+            if pod[0]=='sphere':
+                num_type = 0
+                dummy = 0
+            elif pod[0]=='shell':
+                num_type = 1
+                dummy = 0
+            else: # ellipsoid, x^2/a^2 + y^2/b^2 + z^2/c^2 = 1
+                #
+                # 回転楕円体に対する対称操作について考える。
+                # 回転楕円体はそのままに、プロジェクションする点に対称操作を施して内外判定する方が楽か。
+                # であれば、他のODについてもそのようにした方が良い？
+                #
+                num_type = 2
+                dummy = obj[2]
+            lst_radius_spherical_obj.append([obj[0],obj[1],dummy,num_type])
+            
+        lst_shape.append(pod[0])
+        lst_objs.append(objs1_)
+        lst_pos.append(vnss)
+        lst_atm.append(atom)
+        lst_be.append(be)
+        lst_occ.append(occ)
+        lst_rmax.append(rmax)
+        lst_eshift.append(np.array(eshift,dtype=np.float64))
+        lst_mu.append(np.array(mu,dtype=np.float64))
+        
+        #-----------------------------------------------------------------------
+        # symmetry operation on x1,x2,x3 for each subdevided OD. 
+        # used for shift vectors, i.e. xeshift
+        #-----------------------------------------------------------------------
+        ve1=projection_sets_par_numerical_normalized(x1)
+        ve2=projection_sets_par_numerical_normalized(x2)
+        ve3=projection_sets_par_numerical_normalized(x3)
+        #
+        flg='normal'
+        # in the independent OD (obj)
+        v1_=generator_obj_symmetric_vector_specific_symop_1(ve1,indx_site_sym,flg) # 5f
+        v2_=generator_obj_symmetric_vector_specific_symop_1(ve2,indx_site_sym,flg) # 3f
+        v3_=generator_obj_symmetric_vector_specific_symop_1(ve3,indx_site_sym,flg) # 2f
+        #
+        # in the ODs at equivalent positions
+        v1_=generator_obj_symmetric_vectors_specific_symop_1(v1_,indx_coset,flg) # 5f
+        v2_=generator_obj_symmetric_vectors_specific_symop_1(v2_,indx_coset,flg) # 3f
+        v3_=generator_obj_symmetric_vectors_specific_symop_1(v3_,indx_coset,flg) # 2f
+        #
+        lst_xe1.append(v1_)
+        lst_xe2.append(v2_)
+        lst_xe3.append(v3_)
+        
+        #-----------------------------------------------------------------------
+        # symmetry operation on x1,x2,x3 for each subdevided OD. 
+        # used for axial vectors, i.e. magnetic moment
+        #-----------------------------------------------------------------------
+        # in the independent OD (obj)
+        flg='axial'
+        #flg='normal'
+        v1_=generator_obj_symmetric_vector_specific_symop_1(ve1,indx_site_sym,flg) # 5f
+        v2_=generator_obj_symmetric_vector_specific_symop_1(ve2,indx_site_sym,flg) # 3f
+        v3_=generator_obj_symmetric_vector_specific_symop_1(ve3,indx_site_sym,flg) # 2f
+        #-----------------------------------------------------------------------
+        # in the ODs at equivalent positions
+        v1_=generator_obj_symmetric_vectors_specific_symop_1(v1_,indx_coset,flg) # 5f
+        v2_=generator_obj_symmetric_vectors_specific_symop_1(v2_,indx_coset,flg) # 3f
+        v3_=generator_obj_symmetric_vectors_specific_symop_1(v3_,indx_coset,flg) # 2f
+        #-----------------------------------------------------------------------
+        lst_mxe1.append(v1_)
+        lst_mxe2.append(v2_)
+        lst_mxe3.append(v3_)
+        
+        if test_flag==0:
+            pass
+        else:
+            #-----------------------------------------------------------------------
+            # symmetry operation on x1,x2,x3 for each subdevided OD. 
+            # used for axial vectors, i.e. magnetic moment
+            #-----------------------------------------------------------------------
+            # in the independent OD (obj)
+            flg='axial'
+            #flg='normal'
+            x1_=generator_obj_symmetric_vector_specific_symop_1(x1,indx_site_sym,flg) # 5f
+            x2_=generator_obj_symmetric_vector_specific_symop_1(x2,indx_site_sym,flg) # 3f
+            x3_=generator_obj_symmetric_vector_specific_symop_1(x3,indx_site_sym,flg) # 2f
+            # in the ODs at equivalent positions
+            x1_=generator_obj_symmetric_vectors_specific_symop_1(x1_,indx_coset,flg) # 5f
+            x2_=generator_obj_symmetric_vectors_specific_symop_1(x2_,indx_coset,flg) # 3f
+            x3_=generator_obj_symmetric_vectors_specific_symop_1(x3_,indx_coset,flg) # 2f
+            #-----------------------------------------------------------------------
+            lst_mx1.append(x1_)
+            lst_mx2.append(x2_)
+            lst_mx3.append(x3_)
+    print('lst_objs = ',len(lst_objs))
+    print('lst_radius_spherical_obj = ',len(lst_radius_spherical_obj))
+    
+    return 
+    
+def gen_structure(aico, brv, window, nmax, oshift, x1, x2, x3, verbose = 0, test_flag = 0):
+    """
+    generating atomic arrangement by projection method.
+    """
+    return
+    
 def strc(aico, brv, model, nmax, oshift, x1, x2, x3, verbose = 0, test_flag = 0):
     """
     this function generates atomic and magnetic structures in 3-d physical space.
@@ -122,6 +320,8 @@ def strc(aico, brv, model, nmax, oshift, x1, x2, x3, verbose = 0, test_flag = 0)
     lst_mxe3=[]
     lst_eshift=[]
     #lst_sphere_radius=[]
+    
+    lst_radius_spherical_obj=[]
     
     #### TEST ####
     if test_flag==0:
@@ -221,15 +421,18 @@ def strc(aico, brv, model, nmax, oshift, x1, x2, x3, verbose = 0, test_flag = 0)
                 #
                 #obj=generator_obj_symmetric_obj(pod[1],position)
                 #
+                #
+                # Spherical approximation of the OD (tmp) to a spherical OD.
+                radius_spherical_obj = spherical_approximation_obj(obj)
+                print('   radius of spherically approximated obj: %8.6f, %8.6f'%(radius_spherical_obj[0],radius_spherical_obj[1]))
+                lst_radius_spherical_obj.append(radius_spherical_obj)
+                #lst_sphere_radius.append(rad_obj)
+                #
+                #
                 #obj=shift_object(obj,position)
                 #obj=generator_obj_symmetric_obj_specific_symop(obj,position,indx_site_sym)
                 obj=generator_obj_symmetric_obj_specific_symop(obj,V0,indx_site_sym)
                 #
-                # Spherical approximation of the OD (tmp) to a spherical OD.
-                if verbose>1:
-                    radius_spherical_obj = spherical_approximation_obj(obj)
-                    print('   radius of spherically approximated obj: %8.6f'%(radius_spherical_obj))
-                #lst_sphere_radius.append(rad_obj)
                 #
                 #num_tetrahedron=len(obj)
                 #num_coset=len(indx_coset)
@@ -340,9 +543,13 @@ def strc(aico, brv, model, nmax, oshift, x1, x2, x3, verbose = 0, test_flag = 0)
                 # WIP
                 pass
         else: # for spherical model
-            # WIP
-            pass
+            lst_radius_spherical_obj.append(obj)
             
+            pass
+    
+    print('lst_objs = ',len(lst_objs))
+    print('lst_radius_spherical_obj = ',len(lst_radius_spherical_obj))
+    
     print('Generating atomic structure...')
     lst=[]
     for h1 in range(-nmax,nmax+1):
@@ -386,6 +593,11 @@ def strc(aico, brv, model, nmax, oshift, x1, x2, x3, verbose = 0, test_flag = 0)
                                         wi=posi[i3]
                                         point=vi+wi-oshift # oshift: shift of cut space along Eperp space
                                         for i4,obj3 in enumerate(obj2): # symmetric OD
+                                            #
+                                            # ここに球体の内外判定を挟む
+                                            if not inside_outside_shpere(point,lst_radius_spherical_obj[i1]):
+                                                continue
+                                            #
                                             xe1_=xe1[i3][i4]
                                             xe2_=xe2[i3][i4]
                                             xe3_=xe3[i3][i4]
@@ -466,10 +678,14 @@ def spherical_approximation_obj(obj):
     for i1,vt in enumerate(vertices):
         #a=get_internal_component_numerical(vt)-position
         a=get_internal_component_numerical(vt)
-        dd=np.sqrt(a[0]**2+a[1]**2+a[2]**2)
+        #dd=np.sqrt(a[0]**2+a[1]**2+a[2]**2)
+        dd=np.linalg.norm(a)
         lst.append(dd)
     #return [max(lst),position]
-    return max(lst)
+    if min(lst) > 1e-1:
+        return min(lst),max(lst)
+    else:
+        return 0,max(lst)
     
 def spherical_approximation_tetrahedron(tetrahedron):
     """
@@ -480,13 +696,20 @@ def spherical_approximation_tetrahedron(tetrahedron):
     dd1=ball_radius(tetrahedron,cen1)
     return cen1,dd1
     
-def inside_outside_shpere(point,radius,postion):
+def inside_outside_shpere(point,radius):
     """
     numerical calc.
+    Judge whether the 'point' is inside or out side side of a sherical 
+    occupation domain with the 'radius' centred at 0,0,0,0,0,0.
+    
+    input:
+        point, ndarray
+        radius, float
+    output:
+    
     """
-    p=point-postion
     #if np.sqrt(p[0]**2+p[1]**2+p[2]**2)<=radius:
-    if np.linalg.norm(p)<=radius:
+    if radius[0]<=np.linalg.norm(point)<=radius[1]:
         return True # inside
     else:
         return False # outside
@@ -532,7 +755,44 @@ if __name__ == '__main__':
         print('inside')
     else:
         print('outside')
-        
+    
+    
+    #------------------------
+    print('TEST: strc() with sperical model')
+    #------------------------
+    # def strc(aico, brv, model, nmax, oshift, x1, x2, x3, verbose = 0, test_flag = 0):
+    
+    aico = 5.689
+    brv = 'p'
+    oshift = 0
+    x1 = [1, 0, 0, 0, 0, 0] #5f
+    x2 = [1, 0,-1, 0,-1, 0] #3f
+    x3 = [1, 0, 0, 0,-1, 0] #2f
+    verbose = 0
+    test_flag = 0
+    
+    POS_V  = np.array([ 0, 0, 1],[ 0, 0, 1],[ 0, 0, 1],[ 0, 0, 1],[ 0, 0, 1],[ 0, 0, 1])
+    POS_C  = np.array([ 1, 0, 2],[ 1, 0, 2],[ 1, 0, 2],[ 1, 0, 2],[ 1, 0, 2],[ 1, 0, 2])
+    POS_EC = np.array([ 1, 0, 2],[ 0, 0, 1],[ 0, 0, 1],[ 0, 0, 1],[ 0, 0, 1],[ 0, 0, 1])
+    elm_A = 'Yb'
+    elm_B = 'Cd'
+    
+    myModel = {}
+    #myModel[0] = [elm_A, ['polyhedron', od1_asym],      POS_V,  be, 0]
+    #            element,    OD,  OD shape,  coordinate, be. mu(magnetic moment)
+    myModel[0] = [elm_A, ['sphere',   [8.1]],      POS_V,  be, 0]
+    myModel[1] = [elm_B, ['sphere',   [5.8]],      POS_C,  be, 0]
+    myModel[2] = [elm_A, ['shell',    [5.8, 9.2]], POS_C,  be, 0]
+    myModel[3] = [elm_A, ['ellipsoid',[5.0, 9.0]], POS_EC, be, 0]
+    
+    model = myModel
+    
+    x1=np.array(x1,dtype=np.float64) #5f
+    x2=np.array(x2,dtype=np.float64) #3f
+    x3=np.array(x3,dtype=np.float64) #2f
+    
+    
+    
     
     
     """
